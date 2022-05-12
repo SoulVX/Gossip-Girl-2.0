a87a9951adc2b54b39a66b67b5df0122c1d9d2fd
Radu002 <nedelcuradustefan00@gmail.com>
2022-05-13 00:55:03 +0300

    metode + exceptii

---
+++ src/main/java/com/stefanp/springboot/exception/InvalidGossipException.java
@@ -0,0 +1,10 @@
+package com.stefanp.springboot.exception;
+
+import com.stefanp.springboot.model.Gossip;
+
+public class InvalidGossipException extends RuntimeException {
+
+    public InvalidGossipException(Gossip gossip) {
+        super(String.format("Gossip with id %s is inappropriate!", gossip.getId()));
+    }
+}
