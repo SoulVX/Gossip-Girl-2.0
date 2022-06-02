cabea70a60c5df081f69ffdd8ac425680e4eedff
Ovidiu-Daniel Arvat <57312446+iDanyel@users.noreply.github.com>
2022-06-02 22:08:26 +0300

    Truth frontend

--- src/main/java/com/springboot/controller/TruthController.java
+++ src/main/java/com/springboot/controller/TruthController.java
@@ -1,20 +1,26 @@
 package com.springboot.controller;
 
+import com.springboot.model.Gossip;
 import com.springboot.model.Truth;
 import com.springboot.service.TruthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class TruthController {
     private final TruthService truthService;
+    private ArrayList<Truth> feed;
 
     public TruthController(TruthService truthService) {
         this.truthService = truthService;
+
+        feed = new ArrayList<>();
     }
 
     @PostMapping("/api/truth")
@@ -31,5 +37,18 @@ public class TruthController {
     @GetMapping("/api/truth")
     public List<Truth> getAllTruths() {
         return truthService.getAllTruths();
+    }
+
+    @PostMapping("/postTruth")
+    public String postTruth(Truth truth) {
+        truthService.saveTruth(truth);
+        feed.add(truth);
+        return "redirect:/truthFeed";
+    }
+
+    @GetMapping("/truthFeed")
+    public String showFeed(Model model) {
+        model.addAttribute("truthFeed", feed);
+        return "/truthFeed";
     }
 }
